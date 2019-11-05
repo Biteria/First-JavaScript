7d54d0fd7e266b7d47a0516d2d02eaf1204e6371
Biteria <56840604+Biteria@users.noreply.github.com>
2019-11-05 10:36:01 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+console. log('asdf');
+console.log(1235);
+
+const PI = 3.141592654
+console.log(PI)
+
+const vardas = " Abc";
+console.log(vardas)
+
+let amziausVidurkis = 33;
+console.log(amziausVidurkis)
+
+let oras = 'vesus' + ' lietingas';
+console.log(oras);
+
+const oras2= "'vasara silta' ir  'ziema salta'";
+console.log(oras2);
+// komentarai easy;
+const suma = amziausVidurkis + 2;
+console.log(suma);
+
+const tekstas = 'sukasi' + ' planetos';
+console.log(tekstas);
+
+const skaitmenys = [PI + 11 * 56];
+console.log(skaitmenys);
